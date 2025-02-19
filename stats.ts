d6dee2e2e5a22b97fa0acbffe519dcedba68022b
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-19 19:27:45 +0800

    feat: add downloadsText

--- stats.ts
+++ stats.ts
@@ -3,14 +3,19 @@ import pkg from './package.json' with { type: 'json' }
 
 const deps = Object.keys(pkg.dependencies)
 
-const data = await Promise.all(
-  deps.map((dep) =>
-    fetch(`https://api.npmjs.org/downloads/point/last-week/${dep}`).then((r) =>
-      r.json(),
+const data = (
+  await Promise.all(
+    deps.map((dep) =>
+      fetch(`https://api.npmjs.org/downloads/point/last-week/${dep}`).then(
+        (r) => r.json(),
+      ),
     ),
-  ),
+  )
 )
-
-data.sort((a, b) => b.downloads - a.downloads)
+  .map((item) => ({
+    downloadsText: item.downloads.toLocaleString(),
+    ...item,
+  }))
+  .sort((a, b) => b.downloads - a.downloads)
 
 await writeFile('stats.json', `${JSON.stringify(data, null, 2)}\n`)
