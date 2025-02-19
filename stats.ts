124d36232503865784321d4498892537fe490845
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-19 19:19:19 +0800

    feat: save npm downloads count

---
+++ stats.ts
@@ -0,0 +1,16 @@
+import { writeFile } from 'node:fs/promises'
+import pkg from './package.json' with { type: 'json' }
+
+const deps = Object.keys(pkg.dependencies)
+
+const data = await Promise.all(
+  deps.map((dep) =>
+    fetch(`https://api.npmjs.org/downloads/point/last-week/${dep}`).then((r) =>
+      r.json(),
+    ),
+  ),
+)
+
+data.sort((a, b) => b.downloads - a.downloads)
+
+await writeFile('stats.json', `${JSON.stringify(data, null, 2)}\n`)
