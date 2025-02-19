56085e0d6bc72aa78846e9d9182a8c1fe6bca223
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-19 18:45:16 +0800

    feat: add config

---
+++ node-modules-inspector.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'node-modules-inspector'
+
+export default defineConfig({
+  defaultFilters: {
+    sourceType: 'prod',
+    excludeWorkspace: true,
+  },
+  defaultSettings: {
+    showPublishTimeBadge: true,
+    showInstallSizeBadge: true,
+    showFileComposition: true,
+  },
+  excludeDependenciesOf: [
+    'eslint',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/utils',
+  ],
+  excludePackages: ['typescript', 'pnpm'],
+})
