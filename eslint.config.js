1765ed5a5e0ca04435a072062014a405cb166cb7
Kevin Deng <sxzz@sxzz.moe>
2026-05-02 15:28:41 +0900

    chore: fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
 import { sxzz } from '@sxzz/eslint-config'
 
-export default sxzz({ pnpm: true })
+export default sxzz({
+  pnpm: true,
+  baseline: {
+    ignoreFeatures: ['top-level-await'],
+  },
+})
